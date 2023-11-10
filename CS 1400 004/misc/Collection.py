776a8659fa5a5bb8174ed9b9bccb5cbeccb5bab3
Ryan Schultz <ryan.s.schultz0@gmail.com>
2023-11-09 23:22:30 -0700

    Cs

--- CS 1400 004/misc/Collection.py
+++ CS 1400 004/misc/Collection.py
@@ -84,6 +84,7 @@ if __name__ == "__main__":
     main()
 '''
 #####################################################################################################  Tuples
+'''
 values = (False, True, False, False, False)
 print("Are all values true values?:", all(values)) # all = if all are true, any = if any are true
 
@@ -113,6 +114,124 @@ candy_names_unzip, candy_prices_unzip = zip(*candy_names_prices)
 
 print("Candy prices after unzipping Tuple 'candy_names_prices':", candy_prices_unzip)
 print("Candy names after unzipping Tuple 'candy_names_prices':", candy_names_unzip)
+'''
+#####################################################################################################  Dicktionarys
+
+watches = {
+  'Speedmaster' : 'Omega',
+  'Submariner' : 'Rolex',
+  'Tank' : 'Cartier'
+}                           # the items in the left of the : are known as keys, The items on the right are values, and itmes are everything
+
+keys = list(watches.keys())
+print(type(keys))
+print(keys)
+values = list(watches.values())
+print(type(values))
+print(values)
+items = list(watches.items())
+print(type(items))
+print(items)
+
+print(watches.get('Tank')) # will output the items related to that key or none if not there
+
+watches.setdefault('Royal Oak') # like get but will make new things in the dictionry if they wern't already there
+print(watches)
+
+print(watches.pop('Submariner')) # pop completly removes the thing from the dictionary
+print(watches)
+
+print(watches.popitem()) # like pop but removes the last thing in the dictionary
+print(watches)
+
+del watches['Submariner'] # delets the specified thing
+print(watches)
+
+watches.clear() # delets the entire dictionary
+print(watches)
+
+watches.update(Speedmaster='Swatch Group', Tank='Richemont') # update adds things to the dictionary
+print(watches)
+
+watches1 = {
+  'Speedmaster' : 'Omega',
+  'Submariner' : 'Rolex',
+  'Tank' : 'Cartier'
+}
+
+watches2 = watches1.copy()      # copy
+watches1['Submariner'] = 'Tudor'
+
+print(watches1)
+print(watches2)
+
+
+#1
+def find_key(d, v):
+  keys = list(d.keys())
+  values = list(d.values())
+  index = values.index(v)
+  return keys[index]
+
+#2
+
+def move_to_bottom(d, k):
+  if k not in d:
+    return 'The key is not in the dictionary'
+  else:
+    value = d.pop(k)
+    d[k] = value
+    return d
+  
+#3
+
+def swap(d):
+  keys = d.keys()
+  values = d.values()
+  swapped_tuples = zip(values, keys)
+  value_types = [type(elem) for elem in values]
+  
+  if type({}) in value_types or type([]) in value_types:
+    return 'Cannot swap the keys and values for this dictionary'
+  else:
+    new_dict = dict(swapped_tuples)
+    return new_dict
+
+#4
+
+def is_nested(d):
+  values = d.values()
+  value_types = [type(elem) for elem in values]
+  if type(()) in value_types or type([]) in value_types or type({}) in value_types:
+    return True
+  else:
+    return False
+
+#5
+
+import json
+
+def compare(f1, f2):
+  with open(f1) as file1, open(f2) as file2:
+    data1 = json.load(file1)
+    data2 = json.load(file2)
+    if data1 == data2:
+      return 'The dictionaries are equal'
+    else:
+      count1 = len(data1)
+      count2 = len(data2)
+      if count1 > count2:
+        return 'Dictionary 1 is longer than dictionary 2'
+      elif count2 > count1:
+        return 'Dictionary 2 is longer than dictionary 1'
+      else:
+        return 'Dictionary 1 and dictionary 2 have the same length'
+      
+#6
+
+
+
+
 
 
 
